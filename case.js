ce0ab5a9e6933d7b78f52065d619206a22502ebd
LEBALOC2002 <108645216+LEBALOC2002@users.noreply.github.com>
2022-08-05 14:47:45 +0700

    MANAGE-PLAYERS-LIST

--- case.js
+++ case.js
@@ -1,6 +1,6 @@
 var idRow;
 function Player
- (id, name, photo, birthday, nation) {
+    (id, name, photo, birthday, nation) {
     this.id = id;
     this.name = name;
     this.photo = photo;
@@ -8,10 +8,7 @@ function Player
     this.nation = nation;
 }
 let id = 0;
-let players = [
-    new Player(++id, "Jadon Malik Sancho ", "https://media.vov.vn/sites/default/files/styles/large/public/2021-07/sancho_mu.jpg", "25-03-2000", "Vương Quốc Anh"),
-    new Player(++id, "Cristiano Ronaldo", "https://media-cdn-v2.laodong.vn/Storage/NewsPortal/2021/10/30/969136/Cristiano-Ronaldo4.jpg", "05-02-1985", "Bồ Đào Nha")
-]
+let players = [] ;
 function renderlistplayer() {
     let htmls = players.map(function (list) {
         return `
@@ -24,8 +21,8 @@ function renderlistplayer() {
         <td>${list.birthday}</td>
         <td>${list.nation}</td>
         <td> 
-        <button class="d-none" id="eventedit" onclick="edit(${list.id -1 })">EDIT</button>
-        <button id="eventdelete" onclick="deletelist(${list.id })">DELETE</button>
+        <button class="td-tbcass" id="buttonedit" onclick="edit(${list.id - 1})">EDIT</button>
+        <button class="td-tbcase" id="buttondelete" onclick="deletelist(${list.id})">DELETE</button>
         </td>
         </tr>
         `
@@ -35,11 +32,52 @@ function renderlistplayer() {
 }
 function add() {
     let name = document.querySelector(".name").value;
+    if ( name.trim() == "" || name == null){
+        alert ("Please re-enter your name");
+        return ; 
+    }
     let photo = document.querySelector(".photo").value;
-    let birthday = document.querySelector(".birthday").value;
-    let nation = document.querySelector(".nation").value;
+    if ( photo.trim() == "" || photo == null){
+        alert ("Please re-enter your name");
+        return ; 
+    }
+    if(!isURL(photo)){
+        alert('Photo must url');
+        return;
+    }
+    let birthday = document.querySelector(".birthday").value ;
+    if ( birthday.trim() == "" || birthday == null){
+        alert ("Please re-enter your Photo");
+        return ; 
+    }
+    let nation = document.querySelector(".nation").value ;
+    if ( nation.trim() == "" || nation == null){
+        alert ("Please re-enter your Nation");
+        return ; 
+    }
     players.push(new Player(++id, name, photo, birthday, nation));
+    setdata(keydata,players);
     renderlistplayer();
+    clear ();
+}
+function clear (){
+    document.querySelector(".name ").value = null;
+    document.querySelector(".photo ").value = null;
+    document.querySelector(".birthday ").value = null;
+    document.querySelector(".nation").value = null;
+}
+function deletelist(id) {
+    let index = players.findIndex(function (player) {
+        return player.id == id;
+    })
+    confirmed = window.confirm(`Bạn có chắc chắn muốn xóa ${players[index].name} không?`);
+    if (confirmed) {
+
+        players.splice(index, 1);
+        setdata(keydata,players);
+        renderlistplayer(players);
+    }
+    deletelist();
 }
 function edit(index) {
     idRow = index;
@@ -49,32 +87,54 @@ function edit(index) {
     document.getElementById('input3').value = part.birthday;
     document.getElementById('input4').value = part.nation;
 }
-function deletelist(id) {
-    let index = players.findIndex(function(player){
-        return player.id == id;
-    })
-    confirmed = window.confirm(`Bạn có chắc chắn muốn xóa ${players[index].name} không?`);
-    if (confirmed) {
-       
-        players.splice(index,1);
-        renderlistplayer(players);
-    }
-    deletelist();
-}
 function cancel() {
- document.querySelector(".name").value = "";
- document.querySelector(".photo").value = "";
- document.querySelector(".birthday").value = "";
- document.querySelector(".nation").value = "" ;
+    document.querySelector(".name").value = "";
+    document.querySelector(".photo").value = "";
+    document.querySelector(".birthday").value = "";
+    document.querySelector(".nation").value = "";
 }
-function save (id){
-    let number = id +1 ;
+function save(id) {
+    let number = i;
     let name = document.getElementById('input1').value;
-    let photo =  document.getElementById('input2').value;
+    let photo = document.getElementById('input2').value;
     let birthday = document.getElementById('input3').value;
     let nation = document.getElementById('input4').value;
     players[id] = new Player(number, name, photo, birthday, nation);
+    setdata(keydata,players);
     renderlistplayer();
 
 }
-renderlistplayer();
+let keydata = 'keydata';
+function getdata (key){
+ return JSON.parse(localStorage.getItem(key));
+}
+function setdata (key,data){
+ localStorage.setItem(key,JSON.stringify(data));
+}
+function dataplayer() {
+    if (localStorage.getItem(keydata) == null) {
+        players = [
+            new Player(++id, "Jadon Malik Sancho ", "https://media.vov.vn/sites/default/files/styles/large/public/2021-07/sancho_mu.jpg", "25-03-2000", "Vương Quốc Anh"),
+            new Player(++id, "Cristiano Ronaldo", "https://media-cdn-v2.laodong.vn/Storage/NewsPortal/2021/10/30/969136/Cristiano-Ronaldo4.jpg", "05-02-1985", "Bồ Đào Nha")
+        ];
+        setdata(keydata,players);
+    }
+    else {
+        players = getdata(keydata)
+    }
+}
+
+function isURL(urlString){
+    try{
+        new URL(urlString);
+        return true;
+    }
+    catch{
+        return false;
+    }
+}
+function main() {
+    dataplayer();
+    renderlistplayer();
+}
+main();
